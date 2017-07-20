07cfe56201a8601d6dcb2b9b20db51d85f650401
Liam Lawrence <and3212@gmail.com>
2017-07-20 15:21:26 -0400

    Updated my grapher

--- src/python/GraphManager.py
+++ src/python/GraphManager.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 
 def graph(theta, r, angle, offset, type):
     ax = plt.subplot(111, polar=True)
-    ax.plot(theta, r, color='r', linewidth=3, label='another line')
+    ax.plot(theta, r, color='r', linewidth=3, label='Radiation Path')
     ax.grid(True)
-
     ax.set_title(type + " Scan at " + str(angle) + "°", va='bottom')
-    #ax.set_title("R has an offset of " + str(offset), va='top')
+
+    # if offset != 0:
+        # ax.text(200, 55, r"R has an offset of " + str(offset), fontsize=12, fontweight='bold')
     ax.legend(bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
     plt.show()
